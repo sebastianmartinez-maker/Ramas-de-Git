6a71cb4b398ec5bae9b6fe40b36de8615aa6eea9
Sebastián <eduardomartinez@manitasalaobraeducate.edu.gt>
2025-10-27 21:30:04 -0600

    Septimo

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-print("Hola desde maingit")
-print("Hola desde rama 2")
-"""
-
-
-"""
-
-num = int(input("Ingresa qué tabla quieres"))
-for i in range(1,11):
-    print(f"{num} X {i} = {num * i}")
-print(0)
+print("Hola desde rama 3")
