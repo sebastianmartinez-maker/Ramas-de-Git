6ea4d623833d1338b68a8b9031d9de334bb7e7d1
Sebastián <eduardomartinez@manitasalaobraeducate.edu.gt>
2025-10-27 21:41:42 -0600

    Octavo

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+print("Hola desde main")
+print("Hola desde rama 2")
+"""
+
+
+"""
+
+num = int(input("Ingresa qué tabla quieres"))
+for i in range(1,11):
+    print(f"{num} X {i} = {num * i}")
 print("Hola desde rama 3")
